992b39ac14da19df7932f8bcc6181442c88c68dd
Олег Дробіт <olegdrobit16@gmail.com>
2026-05-09 15:34:56 +0300

    Reset project to default stureture

--- lib/features/schedule/domain/repositories/schedule_repository.dart
+++
@@ -1,5 +0,0 @@
-import '../entities/lesson_entity.dart';
-
-abstract class ScheduleRepository {
-  Future<List<LessonEntity>> getScheduleForGroup(String groupId);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'features/schedule/presentation/bloc/schedule_bloc.dart';
-import 'features/schedule/data/datasources/schedule_local_datasource.dart';
-import 'features/schedule/data/repositories/schedule_repository_impl.dart';
-import 'features/schedule/presentation/page/home_page.dart';
 
 void main() {
-  final dataSource = ScheduleLocalDataSourceImpl();
-  
-  final repository = ScheduleRepositoryImpl(localDataSource: dataSource);
+  runApp(const MyApp());
+}
 
-  runApp(
-    MaterialApp(
-      home: BlocProvider(
-        create: (context) => ScheduleBloc(repository: repository),
-        child: const HomePage(),
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-    ),
-  );
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
